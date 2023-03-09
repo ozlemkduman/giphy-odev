64559b5686a6faf18b18db74573d35249b041d0c
ozlemkduman <ozlemk.duman@gmail.com>
2023-03-09 11:17:58 +0300

    add new script

--- script.js
+++ script.js
@@ -17,16 +17,17 @@ function getGiphy(e) {
       const arrData = data.data;
       arrData.forEach((item) => {
         if (item.title.trim() == inputDOM.value.trim()) {
-          resultsDOM.innerHTML="";
+          resultsDOM.innerHTML = "";
           const divItem = document.createElement("div");
           divItem.className = "item";
           resultsDOM.append(divItem);
           const imgDOM = document.createElement("img");
           const imgTitle = document.createElement("p");
-          imgTitle.className="imgTitle"
+          imgTitle.className = "imgTitle";
           divItem.append(imgDOM, imgTitle);
           imgDOM.src = item.images.fixed_width.url;
           imgTitle.textContent = item.title;
+          console.log(item.title);
         }
 
         console.log(item.title);
@@ -36,3 +37,34 @@ function getGiphy(e) {
       console.log(err);
     });
 }
+
+function main() {
+  fetch(
+    "https://api.giphy.com/v1/gifs/trending?api_key=UPZZq43RmUOLkGFHqFdbAY7FUsZ2yDL2"
+  )
+    .then((item) => {
+      return item.json();
+    })
+    .then((data) => {
+      const arrData = data.data;
+      arrData.forEach((item) => {
+        const divItem = document.createElement("div");
+        divItem.className = "item";
+        resultsDOM.append(divItem);
+        const imgDOM = document.createElement("img");
+        const imgTitle = document.createElement("p");
+        imgTitle.className = "imgTitle";
+        divItem.append(imgDOM, imgTitle);
+        imgDOM.src = item.images.fixed_width.url;
+        imgTitle.textContent = item.title;
+
+        console.log(item.title);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+
+main();
